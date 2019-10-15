41fbc03efcb35c9901e017c5b04bf30ba86e05fc
doot <1306793135@qq.com>
2019-10-15 23:01:29 +0800

    nearly finish ch02_exp.

--- ch02exp/ch02exp.h
+++ ch02exp/ch02exp.h
@@ -10,67 +10,623 @@
  */
 
 #include <iostream>
-
-void error(const std::string & msg)
-{
-    std::cerr << "error, msg : " << msg;
-    while(1);
-}
-
-
+#include <string>
+#include <cstring>
+#include <exception>
 
 
 template<typename T>
 class SeqList
 {
-    // "m_" means "member"
-private:
     T* m_ptr;
-    size_t m_allocated_size;
     size_t m_size;
-public:
-    SeqList(size_t n);
-    ~SeqList():
+    size_t m_alloc_size;
 
-    bool insertAtBack(const T& data);
 
-    bool insertAtIndex(size_t index, const T& data);
+    // it will acquire a new space and
+    // move the elements to there
+    // then release the old memory
+    void acquireMemory(size_t alloc_size);
+
+    // will not clear memory allocated
+    size_t binaryFind(const T& v);
+
+  public:
+    SeqList();
+    SeqList(size_t pre_size);
+    ~SeqList();
+    SeqList(const SeqList& s);
+
+
+    // get the back element
+    // WARNING : there are no check
+    T& back() { return m_ptr[m_size-1];};
+
+
+    // insert an element at 'index'
+    bool insertAtIndex(const T& v, size_t index);
+
+    // insert an element at back of SeqList
+    // same as "insertAtIndex(v, m_size)"
+    void insertAtBack(const T& v);
+
     bool removeAtIndex(size_t index);
 
+    bool uniqueOrderedInsert(const T& v);
+
+
+    bool orderedInsert(const T& v);
+    bool uniqueInsert(const T& v);
+
+
+    bool remove(const T& v);
+
+
+    size_t size() const {return m_size;};
+
+    // clear all the acquired memory
+    void clear();
+
+    // resize SeqList to length 
+    // m_size = length
+    // and elements beyond length will be discarded
+    bool resize(size_t length);
+
     T& operator[](size_t index);
+    const T& operator[](size_t index)const;
+    SeqList &operator=(const SeqList & s);
+    SeqList operator&(const SeqList & s);
+    SeqList operator|(const SeqList & s);
+    SeqList operator-(const SeqList & s);
 
 };
 
+template<typename T>
+std::ostream &operator<<(std::ostream & os, const SeqList<T> & s)
+{
+    for(size_t i = 0; i < s.size(); ++i)
+    {
+        os << s[i] << ' ';
+    }
+    return os;
+    
+}
 
 template<typename T>
-SeqList<T>::SeqList(size_t n)
+bool SeqList<T>::insertAtIndex(const T& v, size_t index)
 {
-    if(n == 0)
+    if(index > m_size)
     {
-        error("SeqList::SeqList(size_t n), n == 0")
+        return false;
     }
 
-    m_allocated_size = n;
-    m_ptr = new T[n];
+    if(m_size + 1 > m_alloc_size)
+    {
+        acquireMemory((m_size + 1) * 2);
+    }
 
+    size_t i = m_size;
+    for(; index < i; --i)
+    {
+        m_ptr[i] = m_ptr[i-1];
+    }
+    m_ptr[index] = v;
+
+    m_size++;
+    return true;
+}
+
+template<typename T>
+void SeqList<T>::insertAtBack(const T& v)
+{
+    insertAtIndex(v, m_size);
+}
+
+
+template<typename T>
+bool SeqList<T>::removeAtIndex(size_t index)
+{
+    if(index >= m_size)
+    {
+        return false;
+    }
+
+    for(size_t i = index; i < m_size; ++i)
+    {
+        m_ptr[i] = m_ptr[i+1];
+    }
+
+    m_size--;
+
+    return true;
+
+}
+
+template<typename T>
+size_t SeqList<T>::binaryFind(const T& v)
+{
+
+
+    size_t left = 0, right = m_size-1, middle;
+    while(left + 1 < right)
+    {
+        middle = left + (right - left) / 2;
+
+        if(m_ptr[middle] == v)
+        {
+            return middle;
+        }
+
+        if(m_ptr[middle] < v)
+        {
+            left = middle;
+        }
+        else
+        {
+            right = middle;
+        }
+    }
+
+    if(v <= m_ptr[left])
+    {
+        return left;
+    }
+
+    if(v > m_ptr[right])
+    {
+        return right + 1;
+    }
+
+    
+    return right;
+    
+}
+
+// assume the arguments are correct
+template<typename T>
+void SeqList<T>::acquireMemory(size_t alloc_size)
+{
+    auto* old_ptr = m_ptr;
+    m_ptr = new T[alloc_size];
+    m_alloc_size = alloc_size;
+
+    for(size_t i = 0; i < m_size; ++i)
+    {
+        m_ptr[i] = old_ptr[i];
+    }
+    delete[] old_ptr;
+
+}
+
+template<typename T>
+SeqList<T>::SeqList()
+{
+    m_ptr = nullptr;
     m_size = 0;
+    m_alloc_size = 0;
+}
+
+template<typename T>
+SeqList<T>::SeqList(size_t pre_size) : m_ptr(new T[pre_size]), m_size(0), m_alloc_size(pre_size)
+{
+}
+
+template<typename T>
+SeqList<T>::SeqList(const SeqList<T> & s)
+{
+    m_alloc_size = s.m_alloc_size;
+    m_ptr = new T[m_alloc_size];
+    m_size = s.m_size;
+    std::memcpy(static_cast<void*>(m_ptr), static_cast<void*>(s.m_ptr), m_size*sizeof(T));
 }
 
 template<typename T>
 SeqList<T>::~SeqList()
 {
-    delete m_ptr;
+    if(m_ptr != nullptr)
+    {
+        delete[] m_ptr;
+        m_ptr = nullptr;
+        m_size = 0;
+    }
+}
+
+template<typename T>
+bool SeqList<T>::uniqueOrderedInsert(const T& v)
+{
+
+    if(m_size == 0)
+    {
+        return insertAtIndex(v, 0);
+    }
+
+    size_t pos = binaryFind(v);
+
+    if(pos == m_size)
+    {
+        return insertAtIndex(v, m_size);
+    }
+
+    if(m_ptr[pos] < v)
+    {
+        return insertAtIndex(v, pos+1);
+    } else if (m_ptr[pos] > v)
+    {
+        return insertAtIndex(v, pos);
+    } else
+    {
+        return false;
+    }
+}
+
+template<typename T>
+bool SeqList<T>::remove(const T& v)
+{
+    if(m_size == 0)
+    {
+        return false;
+    }
+
+    size_t pos = binaryFind(v);
+
+    if(m_ptr[pos] == v)
+    {
+        return removeAtIndex(pos);
+    }else
+    {
+        return false;
+    }
+}
+
+template<typename T>
+bool SeqList<T>::orderedInsert(const T& v)
+{
+    if(m_size == 0)
+    {
+        return insertAtIndex(v, 0);
+    }
+
+    size_t i = 0;
+
+    if(v < m_ptr[0])
+    {
+        return insertAtIndex(v, 0);
+    }
+
+    while(++i < m_size)
+    {
+        if(m_ptr[i-1] <= v && v < m_ptr[i])
+        {
+            return insertAtIndex(v, i);
+        }
+    }
+
+    
+
+    insertAtIndex(v, m_size);
+}
+
+template<typename T>
+bool SeqList<T>::uniqueInsert(const T& v)
+{
+    if(m_size == 0)
+    {
+        return insertAtIndex(v, 0);
+    }
+
+    size_t pos = binaryFind(v);
+
+    if(pos == m_size)
+    {
+        return insertAtIndex(v, m_size);
+    }
+    else if(m_ptr[pos] == v)
+    {
+        // found element having same value, inserting fails. 
+        return false;
+    }
+
+    return insertAtIndex(v, pos);
 }
 
 
 template<typename T>
-T& SeqList<T>::operator[](size_t index)
+void SeqList<T>::clear()
+{
+    delete[] m_ptr;
+    m_ptr = nullptr;
+    m_size = 0;
+    m_alloc_size = 0;
+}
+
+template<typename T>
+bool SeqList<T>::resize(size_t length)
+{
+    m_size = length;
+    return true;
+}
+
+
+template<typename T>
+T& SeqList<T>::operator[] (size_t index)
 {
     if(index >= m_size)
     {
-        error("SeqList::~SeqList(), index >= m_size;")
+        throw std::invalid_argument("error, SeqList<T>::operator[], index >= m_size");
     }
     return m_ptr[index];
+}
+
+template<typename T>
+const T& SeqList<T>::operator[](size_t index)const
+{
+    if(index >= m_size)
+    {
+        throw std::invalid_argument("error, SeqList<T>::operator[], index >= m_size");
+    }
+    return m_ptr[index];
+}
+
+template<typename T>
+SeqList<T> & SeqList<T>::operator=(const SeqList<T> & s)
+{
+    if(m_alloc_size < s.m_size)
+    {
+        acquireMemory(s.m_size);
+    }
+    memcpy(static_cast<void*>(m_ptr), static_cast<void*>(s.m_ptr), s.m_size * sizeof(T));
+    m_size = s.m_size;
+}
+
+
+
+template<typename T>
+SeqList<T> SeqList<T>::operator&(const SeqList<T> & s)
+{
+    SeqList<T> ret(std::min(m_size, s.m_size));
+    size_t i = 0, j = 0;
+    while(i < m_size && j < s.m_size)
+    {
+        if(m_ptr[i] < s.m_ptr[j])
+        {
+            ++i;
+        } else if (m_ptr[i] > s.m_ptr[j])
+        {
+            ++j;
+        } else
+        {
+            ret.insertAtIndex(m_ptr[i], ret.size());
+            ++i;
+            ++j;
+        }
+    }
+    return ret;
+}
+
+template<typename T>
+SeqList<T> SeqList<T>::operator|(const SeqList<T> & s)
+{
+    SeqList<T> ret(m_size + s.m_size);
+    size_t i = 0, j = 0;
+    while(i < m_size && j < s.m_size)
+    {
+        if(m_ptr[i] < s.m_ptr[j])
+        {
+            ret.insertAtIndex(m_ptr[i], ret.size());
+            ++i;
+        } else if(m_ptr[i] > s.m_ptr[j])
+        {
+            ret.insertAtIndex(s.m_ptr[j], ret.size());
+            ++j;
+        } else
+        {
+            ret.insertAtIndex(m_ptr[i], ret.size());
+            ++i;
+            ++j;
+        }
+    }
+
+    while(i < m_size)
+    {
+        ret.insertAtIndex(m_ptr[i++], ret.size());
+    }
+
+    while(j < s.m_size)
+    {
+        ret.insertAtIndex(s.m_ptr[j++], ret.size());
+    }
+
+    return ret;
+}
+
+template<typename T>
+SeqList<T> SeqList<T>::operator-(const SeqList<T> & s)
+{
+    // a - b
+    // in a but not in b
+    SeqList<T> ret(std::max(m_size, s.m_size));
+    size_t i = 0, j = 0;
+    while(i < m_size && j < s.m_size)
+    {
+        if(m_ptr[i] < s.m_ptr[j])
+        {
+            ret.insertAtIndex(m_ptr[i], ret.size());
+            ++i;
+        } else if(m_ptr[i] > s.m_ptr[j])
+        {
+            ++j;
+        } else
+        {
+            ++i;
+            ++j;
+        }
+    }
+
+    while(i < m_size)
+    {
+        ret.insertAtIndex(m_ptr[i], ret.size());
+        ++i;
+    }
+
+    return ret;
+}
+
+template<typename T>
+void splitByPredicate(const SeqList<T> &orgin, SeqList<T> &t, SeqList<T> &f, bool(*p)(const T& v))
+{
+    for(size_t i = 0; i < orgin.size(); ++i)
+    {
+        if(p(orgin[i]))
+        {
+            t.insertAtBack(orgin[i]);
+        }else
+        {
+            f.insertAtBack(orgin[i]);
+        }
+    }
+}
+
+
+
+// input SeqList must be ordered.
+template<typename T>
+void makeItUnique(SeqList<T> &s)
+{
+
+    if(s.size() == 0)
+    {
+        std::cerr << "error, SeqList empty\n";
+        return;
+    }
+
+    std::cout << "origin SeqList : " << s << std::endl;
+
+    size_t i = 1, j = 0, count = 0;
+
+    //j points to last save element
+
+    while(i < s.size())
+    {
+        if(s[i] != s[j])
+        {
+            ++j;
+            if(j != i)
+            {
+                s[j] = s[i];
+                std::cout << "steps : " << j << ", move '" << s[j] << "' from " << i << " to " << j << std::endl;
+                ++count;
+            }
+        }
+        ++i;
+    }
+
+    s.resize(j+1);
+
+    std::cout << "total move count : " << count << std::endl;
+    std::cout << "result SeqList : " << s << std::endl;
+}
+
+template<typename T>
+void combineIntoSet(SeqList<T> &L1, SeqList<T> &L2)
+{
+    SeqList<T> temp;
+
+    size_t iter1 = 0, iter2 = 0, save = 0;
+    if(L1[0] > L2[0])
+    {
+        temp.insertAtBack(L2[0]);
+        iter2++;
+    }
+    else
+    {
+        temp.insertAtBack(L1[0]);
+        iter1++;
+    }
+
+    while(iter1 < L1.size() && iter2 < L2.size())
+    {
+        if(L1[iter1] < L2[iter2])
+        {
+            if(temp[save] != L1[iter1])
+            {
+                temp.insertAtBack(L1[iter1]);
+                ++save;
+            }
+            ++iter1;
+        } else if(L1[iter1] > L2[iter2])
+        {
+            if(temp[save] != L2[iter2])
+            {
+                temp.insertAtBack(L2[iter2]);
+                ++save;
+            }
+            ++iter2;
+        } else
+        {
+            if(temp[save] != L1[iter1])
+            {
+                temp.insertAtBack(L1[iter1]);
+                ++save;
+            }
+            ++iter1;
+            ++iter2;
+        }
+    }
+
+    while(iter1 < L1.size())
+    {
+        if(temp.back() != L1[iter1])
+        {
+            temp.insertAtBack(L1[iter1]);
+        }
+        ++iter1;
+    }
+    while(iter2 < L2.size())
+    {
+        if(temp.back() != L2[iter2])
+        {
+            temp.insertAtBack(L2[iter2]);
+        }
+        ++iter2;
+    }
+
+    L1 = temp;
+}
+
+
+// if 'sub' if a subset of 's', returning true, else false
+
+template<typename T>
+bool isSubSetOf(SeqList<T> &s, SeqList<T> &sub)
+{
+    size_t i = 0, j = 0;
+
+    while(i < s.size() && j < s.size())
+    {
+        if(s[i] < s[j])
+        {
+
+        } else if (s[i] > s[j])
+        {
+
+        } else
+        {
+            ++i;
+            ++j;
+
+        }
+    }
+
+    if(j < s.size())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
 }
 
 
---
+++ ch02exp/main.cc
@@ -0,0 +1,365 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "ch02exp.h"
+
+
+template<typename T>
+size_t array_size(T& a)
+{
+    return sizeof(a)/sizeof(a[0]);
+}
+
+template<typename T>
+void genSeqSetByTestcase(SeqList<T> & s, T* testcases, size_t n)
+{
+    for(size_t i = 0; i < n; ++i)
+    {
+        s.insertAtBack(testcases[i]);
+    }
+}
+
+void genRandomSeqSet(SeqList<int> &s, size_t n)
+{
+    size_t i = n;
+    while(i--)
+    {
+        s.uniqueOrderedInsert(std::rand()%(2*n));
+    }
+}
+
+
+// test the method "operator[](size_t index0"
+void exp1()
+{
+    SeqList<int> s;
+    for(size_t i = 0; i < 15; ++i)
+    {
+        s.insertAtBack(i);
+    }
+
+
+    size_t testcases[] = {
+        5, s.size(), 0, s.size() + 1, s.size() + 2
+    };
+
+    for(size_t i = 0; i < sizeof(testcases)/sizeof(size_t); ++i)
+    {
+        try
+        {
+            std::cout << "s[" << testcases[i] << "] = " << s[testcases[i]] << std::endl;
+        }catch(std::invalid_argument & e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    SeqList<int> s2;
+    size_t testcases2[] = {
+        0, 2
+    };
+
+    for(size_t i = 0; i < sizeof(testcases2)/sizeof(size_t); ++i)
+    {
+        try
+        {
+            std::cout << "s2[" << testcases2[i] << "] = " << s2[testcases2[i]] << std::endl;
+        }catch(std::invalid_argument & e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+}
+
+void exp2()
+{
+    SeqList<int> s;
+    for(size_t i = 0; i < 15; ++i)
+    {
+        s.insertAtBack(i);
+    }
+
+
+    size_t testcases1[] = {
+        5, s.size(), s.size() + 1, 0, 1, s.size() + 2
+    };
+
+    int x = 100;
+
+
+    for(size_t i = 0; i < sizeof(testcases1) / sizeof(size_t); ++i)
+    {
+        std::cout << "s = " << s << std::endl;
+        bool ret = s.insertAtIndex(x, testcases1[i]);
+
+        std::cout << "insert " << x << " before index : " << testcases1[i];
+        if(ret)
+        {
+            std::cout << ", success";
+        } else
+        {
+            std::cout << ", fail, index out of range";
+        }
+        std::cout << std::endl;
+
+
+    }
+    std::cout << "s = " << s << std::endl;
+
+}
+
+
+void exp3(void)
+{
+    SeqList<int> s;
+    for(size_t i = 0; i < 15; ++i)
+    {
+        s.insertAtBack(i);
+    }
+
+
+    size_t testcases1[] = {
+        5, s.size(), 1, s.size() + 1, 0 
+    };
+
+    for(size_t i = 0; i < sizeof(testcases1) / sizeof(size_t); ++i)
+    {
+        std::cout << "s = " << s << std::endl;
+        std::cout << "delete element at " << testcases1[i];
+
+        bool ret = s.removeAtIndex(testcases1[i]-1);
+
+        if(ret)
+        {
+            std::cout << " , success\n";
+        }
+        else
+        {
+            std::cout << " , fail\n";
+        }
+
+    }
+    std::cout << "s = " << s << std::endl;
+
+    SeqList<int> s2;
+    bool ret = s2.removeAtIndex(5);
+
+}
+
+
+void exp4(void)
+{
+    int testcases_1[] = 
+    {
+        10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+    };
+
+    int testcases_2[] = 
+    {
+        25, 85, 110,8
+    };
+
+
+    SeqList<int> s;
+    genSeqSetByTestcase(s, testcases_1, array_size(testcases_1));
+
+    for(size_t i = 0; i < array_size(testcases_2); ++i)
+    {
+        std::cout << "s = " << s << std::endl;
+        s.orderedInsert(testcases_2[i]);
+        std::cout << "insert " << testcases_2[i] << std::endl;
+
+    }
+    std::cout << "s = " << s << std::endl;
+
+
+}
+
+
+bool isEven(const int &v)
+{
+    return v % 2 == 0;
+}
+
+void exp5(void)
+{
+    SeqList<int> org, odd, even;
+    
+    int testcase1[] = {
+        1,2,3,4,5,6,7,8,9,10,20,30,40,50,60
+    };
+
+    int testcase2[] = {
+        10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+    };
+
+    genSeqSetByTestcase(org, testcase1, sizeof(testcase1) / sizeof(int));
+
+    splitByPredicate(org, even, odd, isEven);
+
+    std::cout << "origin : " << org << std::endl;
+    std::cout << "even : " << even << std::endl;
+    std::cout << "odd : " << odd << std::endl;
+
+
+    org.resize(0);
+    odd.resize(0);
+    even.resize(0);
+
+    genSeqSetByTestcase(org, testcase2, sizeof(testcase2) / sizeof(int));
+
+    splitByPredicate(org, even, odd, isEven);
+
+    std::cout << "origin : " << org << std::endl;
+    std::cout << "even : " << even << std::endl;
+    std::cout << "odd : " << odd << std::endl;
+
+}
+
+void exp6_subtest(int* testcases1, size_t n1, int* testcases2, size_t n2)
+{
+    SeqList<int> L1, L2, L3;
+
+    genSeqSetByTestcase(L1, testcases1, n1);
+    genSeqSetByTestcase(L2, testcases2, n2);
+
+    L3 = L1 & L2;
+
+    std::cout << "L1 = " << L1 << std::endl;
+    std::cout << "L2 = " << L2 << std::endl;
+    std::cout << "L3 = L1 & L2 = " << L3 << std::endl;
+
+
+}
+
+
+void exp6(void)
+{
+    int testcase1_1[] = {
+        1,3,6,10,15,16,17,18,19,20
+    };
+
+    int testcase1_2[] = {
+        1,2,3,4,5,6,7,8,9,10,18,20,30
+    };
+
+    int testcase2_1[] = {
+        1,3,6,10,15,16,17,18,19,20
+    };
+
+    int testcase2_2[] = {
+        2,4,5,7,8,9,12,22
+    };
+
+    int testcase3_1[] = {
+
+    };
+
+    int testcase3_2[] = {
+        1,2,3,4,5,6,7,8,9,10
+    };
+
+    exp6_subtest(testcase1_1, array_size(testcase1_1), testcase1_2, array_size(testcase1_2));
+    exp6_subtest(testcase2_1, array_size(testcase2_1), testcase2_2, array_size(testcase2_2));
+    exp6_subtest(testcase3_1, array_size(testcase3_1), testcase3_2, array_size(testcase3_2));
+
+}
+
+void exp7_sub(int* p, size_t n)
+{
+    SeqList<int> s;
+
+    for(size_t i = 0; i < n; ++i)
+    {
+        s.insertAtBack(p[i]);
+    }
+
+    makeItUnique(s);
+}
+
+
+void exp7(void)
+{
+    int testcase1[] =
+    {
+        1,2,3,4,5,6,7,8,9
+    };
+
+    int testcase2[] =
+    {
+        1,1,2,2,2,3,4,5,5,5,6,6,7,7,8,8,9
+    };
+
+    int testcase3[] = 
+    {
+        1,2,3,4,5,5,6,7,8,8,9,9,9,9,9
+    };
+
+    exp7_sub(testcase1, sizeof(testcase1)/ sizeof(int));
+
+    std::cout << std::endl;
+    
+    exp7_sub(testcase2, sizeof(testcase2)/ sizeof(int));
+
+    std::cout << std::endl;
+
+    exp7_sub(testcase3, sizeof(testcase3)/ sizeof(int));
+
+    std::cout << std::endl;
+
+}
+
+
+void ext_exp1(void)
+{
+
+    SeqList<int> s1;
+    SeqList<int> s2;
+
+    size_t i = 15;
+    while(i--)
+    {
+        s1.orderedInsert(std::rand() % 30);
+        std::cout << "s1 = " << s1 << std::endl;
+        s2.orderedInsert(std::rand() % 30);
+    }
+
+    std::cout << "s1 = " << s1 << std::endl;
+    std::cout << "s2 = " << s2 << std::endl;
+
+    combineIntoSet(s1, s2);
+
+    std::cout << "after combination s1 = " << s1 << std::endl;
+
+}
+
+
+void ext_exp2(void)
+{
+    SeqList<int> s1, s2, s3, s4, s5;
+    
+    genRandomSeqSet(s1, 15);
+    genRandomSeqSet(s2, 15);
+
+    s3 = s1 & s2;
+    s4 = s1 | s2;
+    s5 = s1 - s2;
+
+    std::cout << "s1 = " << s1 << std::endl;
+    std::cout << "s2 = " << s2 << std::endl;
+    std::cout << "s3 = s1 & s2 = " << s3 << std::endl;
+    std::cout << "s4 = s1 | s2 = " << s4 << std::endl;
+    std::cout << "s5 = s1 - s2 = " << s5 << std::endl;
+
+}
+
+int main(void)
+{
+    // initialize the random seed
+    std::srand(std::time(NULL));
+    ext_exp1();
+    exp4();
+    ext_exp2();
+    return 0;
+}
