e7fd336723d537d75785aff1d7cfc5cd6d4bdf03
doot <1306793135@qq.com>
2019-11-26 12:51:29 +0800

    add data set with charcode converted.

--- ch05exp/binarytree/binarytree.cc
+++ ch05exp/binarytree/binarytree.cc
@@ -3,15 +3,5 @@
 
 int main(void)
 {
-    auto node = createBinaryTreeFromFile("bt10.btr");
-
-    auto node2 = treeCopy(node);
-
-    std::cout << std::endl;
-    preOrder(node);
-    std::cout << std::endl;
-    preOrder(node2);
-    std::cout << std::endl;
-
     return 0;
 }
--- ch05exp/binarytree/binarytree.h
+++ ch05exp/binarytree/binarytree.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <queue>
 
 struct BtNode
 {
@@ -110,6 +111,29 @@ void printNodeData(BtNode *node)
     std::cout << node->data << ' ';
 }
 
+std::vector<std::vector<char>> treePath2Predicate(BtNode *root, const std::function<bool(BtNode *)> &predicate)
+{
+    std::vector<std::vector<char>> paths;
+    std::vector<char> path;
+
+    auto pre = [&path](BtNode *root) {
+        path.push_back(root->data);
+    };
+
+    auto mid = [&path, &predicate, &paths](BtNode *root) {
+        if (predicate(root))
+        {
+            paths.push_back(path);
+        }
+    };
+
+    auto post = [&path](BtNode *root) {
+        path.pop_back();
+    };
+
+    advancedOrder(root, pre, mid, post);
+    return paths;
+}
 //========================================================================
 // PROBLEM 1
 auto preOrder = std::bind(advancedOrder, std::placeholders::_1, printNodeData, doNothing, doNothing);
@@ -187,7 +211,7 @@ size_t treeNode2DegCount(BtNode *root)
 // PROBELM 7
 
 // return parent
-void treeShowNearBy(BtNode* result, const char e);
+void treeShowNearBy(BtNode *result, const char e);
 BtNode *treeFind(BtNode *root, const char e)
 {
     BtNode *result = nullptr;
@@ -209,7 +233,7 @@ BtNode *treeFind(BtNode *root, const char e)
     return result;
 }
 
-void treeShowNearBy(BtNode* result, const char e)
+void treeShowNearBy(BtNode *result, const char e)
 {
     if (result)
     {
@@ -265,20 +289,19 @@ void treeShowNearBy(BtNode* result, const char e)
 // PROBELM 8
 
 // return height of "e"
-size_t treeElemHeight(BtNode* root, char e)
+size_t treeElemHeight(BtNode *root, char e)
 {
     size_t result = 0;
     size_t height = 0;
     auto pre = [&height](BtNode *) { ++height; };
     auto post = [&height](BtNode *) { --height; };
-    auto mid = [&height, e, &result](BtNode *root)
-    {
-        if(root->data == e)
+    auto mid = [&height, e, &result](BtNode *root) {
+        if (root->data == e)
         {
             result = height;
         }
     };
-   
+
     advancedOrder(root, pre, mid, post);
     return result;
 }
@@ -292,20 +315,17 @@ std::vector<char> treeLinked2Array(BtNode *root)
     arr.resize(512, '#');
     size_t index = 1;
     size_t count = 1;
-    auto pre = [&arr, &index](BtNode* root)
-    {
+    auto pre = [&arr, &index](BtNode *root) {
         arr[index] = root->data;
-        index *=2;
+        index *= 2;
     };
 
-    auto mid = [&arr, &index, &count](BtNode* root)
-    {
+    auto mid = [&arr, &index, &count](BtNode *root) {
         ++index;
         ++count;
     };
 
-    auto post = [&arr, &index](BtNode* root)
-    {
+    auto post = [&arr, &index](BtNode *root) {
         index /= 2;
     };
 
@@ -313,26 +333,25 @@ std::vector<char> treeLinked2Array(BtNode *root)
     arr.resize(count);
     arr[0] = '#';
     return arr;
-    
 }
 
-BtNode* treeArray2Linked(const std::vector<char>& arr)
+BtNode *treeArray2Linked(const std::vector<char> &arr)
 {
     size_t index = 1;
-    std::vector<BtNode*> nodes;
+    std::vector<BtNode *> nodes;
     nodes.resize(arr.size(), nullptr);
     nodes[0] = new BtNode('#');
 
-    auto p = [](size_t i) { return i/2;};
+    auto p = [](size_t i) { return i / 2; };
 
-    BtNode* tmp;
-    for(size_t i = 1; i < arr.size(); ++i)
+    BtNode *tmp;
+    for (size_t i = 1; i < arr.size(); ++i)
     {
-        if(arr[i] != '#')
+        if (arr[i] != '#')
         {
             tmp = new BtNode(arr[i]);
             nodes[i] = tmp;
-            if(i % 2)
+            if (i % 2)
             {
                 nodes[p(i)]->right = tmp;
             }
@@ -346,14 +365,12 @@ BtNode* treeArray2Linked(const std::vector<char>& arr)
     return nodes[1];
 }
 
-
 //========================================================================
 // PROBELM 10
 
-void treeExchangeLR(BtNode* root)
+void treeExchangeLR(BtNode *root)
 {
-    auto pre = [](BtNode * root)
-    {
+    auto pre = [](BtNode *root) {
         BtNode *tmp = root->left;
         root->left = root->right;
         root->right = tmp;
@@ -362,24 +379,22 @@ void treeExchangeLR(BtNode* root)
     advancedOrder(root, pre, doNothing, doNothing);
 }
 
-
 //========================================================================
 // PROBELM 11
 
-BtNode *treeCopy(BtNode* root)
+BtNode *treeCopy(BtNode *root)
 {
-    std::vector<BtNode*> nodes;
+    std::vector<BtNode *> nodes;
     std::vector<bool> lrs;
-    
+
     bool flag = true;
 
     nodes.push_back(new BtNode('#'));
 
-    auto pre = [&nodes, &flag, &lrs](BtNode *root)
-    {
-        BtNode* tmp = new BtNode(root->data);
-        
-        if(flag)
+    auto pre = [&nodes, &flag, &lrs](BtNode *root) {
+        BtNode *tmp = new BtNode(root->data);
+
+        if (flag)
         {
             nodes.back()->left = tmp;
         }
@@ -394,13 +409,11 @@ BtNode *treeCopy(BtNode* root)
         flag = true; // left
     };
 
-    auto mid = [&nodes, &flag, &lrs](BtNode *root)
-    {
+    auto mid = [&nodes, &flag, &lrs](BtNode *root) {
         flag = false; // right
     };
 
-    auto post = [&nodes, &flag, &lrs](BtNode *root)
-    {
+    auto post = [&nodes, &flag, &lrs](BtNode *root) {
         nodes.pop_back();
         flag = lrs.back();
         lrs.pop_back();
@@ -408,20 +421,121 @@ BtNode *treeCopy(BtNode* root)
 
     advancedOrder(root, pre, mid, post);
 
-    BtNode* ret = nodes[0]->left;
+    BtNode *ret = nodes[0]->left;
     delete nodes[0];
     return ret;
-
 }
 
+//========================================================================
+// PROBELM 12
+
+void treePath2Leaf(BtNode *root)
+{
+    auto paths = treePath2Predicate(root, [](BtNode *r) { return !r->left && !r->right; });
+
+    for (const auto &v : paths)
+    {
+        std::cout << "Leaf : " << v.back() << std::endl;
+        std::cout << "Path : ";
+        for (size_t i = v.size(); i > 0; --i)
+        {
+            std::cout << v[i - 1] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
 
 //========================================================================
-// PROBELM 11
+// PROBELM 13
 
-void treePath2Leaf(BtNode* root)
+void treeHeightOrder(BtNode *root)
 {
+    std::queue<BtNode *> q;
+    q.push(root);
 
+    BtNode *tmp;
+    while (!q.empty())
+    {
+        tmp = q.front();
+        q.pop();
 
+        if (tmp->left)
+        {
+            q.push(tmp->left);
+        }
+
+        if (tmp->right)
+        {
+            q.push(tmp->right);
+        }
+
+        std::cout << tmp->data << ' ';
+    }
+}
+
+//========================================================================
+// PROBELM 14
+
+char treeSharedAncestor(BtNode *root, char e1, char e2)
+{
+    auto vv1 = treePath2Predicate(root, [e1](BtNode *root) { return root->data == e1; });
+    auto vv2 = treePath2Predicate(root, [e2](BtNode *root) { return root->data == e2; });
+
+    if (vv1.size() < 1)
+    {
+        std::cout << "vv1.size < 1\n";
+        return '#';
+    }
+
+    if (vv2.size() < 1)
+    {
+        std::cout << "vv2.size < 1\n";
+        return '#';
+    }
+
+    for (size_t i = 0; i < vv1[0].size() && i < vv2[0].size(); ++i)
+    {
+        if (vv1[0][i] != vv2[0][i])
+        {
+            return vv1[0][i - 1];
+        }
+    }
+    return '#';
+}
+
+//========================================================================
+// PROBELM 15
+
+std::vector<std::vector<char>> treeMaxPathLength(BtNode *root)
+{
+    std::vector<char> tmp;
+    std::vector<std::vector<char>> ret;
+    int max = 0;
+
+    auto pre = [&tmp, &ret](BtNode *root) {
+        tmp.push_back(root->data);
+    };
+
+    auto mid = [&tmp, &ret, &max](BtNode *root) {
+        if(tmp.size() > max)
+        {
+            ret.clear();
+            ret.push_back(tmp);
+            max = tmp.size();
+        }
+        else if(tmp.size() == max)
+        {
+            ret.push_back(tmp);
+        }
+    };
+
+    auto post = [&tmp, &ret](BtNode *root) {
+        tmp.pop_back();
+    };
+
+    advancedOrder(root, pre, mid, post);
+
+    return ret;
 }
 
 #endif //BINARY_TREE_H
