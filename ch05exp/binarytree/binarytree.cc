8c270622189863c29cf99b740922190c065a1a78
doot <1306793135@qq.com>
2019-11-26 12:59:39 +0800

    test github action.

--- ch05exp/binarytree/binarytree.cc
+++ ch05exp/binarytree/binarytree.cc
@@ -3,5 +3,13 @@
 
 int main(void)
 {
+    auto tree = createBinaryTreeFromFile("bt262.btr");
+    preOrder(tree);
+    endl(std::cout);
+    midOrder(tree);
+    endl(std::cout);
+    postOrder(tree);
+    endl(std::cout);
+
     return 0;
 }
